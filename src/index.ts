8a412bd0e2e8fefea100dce8e87806e8efda632a
sadness_ojisan <sadness.ojisan@gmail.com>
2022-08-06 21:57:40 +0900

    Define put get method

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,35 @@
-class Memory {}
+type Cache = {
+  value: unknown;
+  expire: number;
+  timeout: NodeJS.Timeout;
+};
+class Memory {
+  private cache: Record<string, Cache> = {};
+  public put(key: string, value: unknown, time: number) {
+    const timeout = setTimeout(() => {
+      // TODO: delete cache
+    }, time);
+    this.cache[key] = {
+      value,
+      expire: Date.now() + time,
+      timeout,
+    };
+    return value;
+  }
+
+  public get(key: string) {
+    const cached = this.cache[key];
+    if (!cached) return null;
+    if (this.isExpire(cached)) {
+      // TODO: delete cache
+      return null;
+    }
+    return cached;
+  }
+
+  private isExpire(cached: Cache): boolean {
+    return true;
+  }
+}
 
 export default new Memory();
