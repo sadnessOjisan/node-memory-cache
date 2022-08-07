dbab05fb0be1272f7213320e69cf69ce2a75fb23
sadnessOjisan <sadness.ojisan@gmail.com>
2022-08-07 11:08:56 +0900

    Add memory cache instance (#2)
    * Define put get method
    
    * Define delete method
    
    * Add changelog

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,55 @@
-class Memory {}
+type Cache = {
+  value: unknown;
+  expire: number;
+  timeout: NodeJS.Timeout;
+};
+class Memory {
+  private cache: Record<string, Cache> = {};
+  public put(key: string, value: unknown, time: number) {
+    const timeout = setTimeout(() => {
+      this.deleteInternal(key);
+    }, time);
+    this.cache[key] = {
+      value,
+      expire: Date.now() + time,
+      timeout,
+    };
+    return value;
+  }
+
+  public get(key: string) {
+    const cached = this.cache[key];
+    if (!cached) return null;
+    if (this.isExpire(cached)) {
+      this.deleteInternal(key);
+      return null;
+    }
+    return cached;
+  }
+
+  public del(key: string) {
+    const cache = this.cache[key];
+    let canDelete;
+    if (cache) {
+      if (!isNaN(cache.expire) && this.isExpire(cache)) {
+        canDelete = false;
+      }
+    } else {
+      canDelete = false;
+    }
+    if (canDelete) {
+      this.deleteInternal(key);
+    }
+    return canDelete;
+  }
+
+  private deleteInternal(key: string) {
+    delete this.cache[key];
+  }
+
+  private isExpire(cached: Cache): boolean {
+    return cached.expire < Date.now();
+  }
+}
 
 export default new Memory();
